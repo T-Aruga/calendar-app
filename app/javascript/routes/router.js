4625ddc7845d9b92977da9cf251d9d8d4d481e9f
T-Aruga <baideneo@gmail.com>
2020-09-21 09:05:31 +0900

    Add acceess constrain

--- app/javascript/routes/router.js
+++ app/javascript/routes/router.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
+import store from "../store/store.js";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Login from "../views/Login.vue";
@@ -17,11 +18,13 @@ const router = new Router({
       path: "/",
       name: "home",
       component: Home,
+      meta: { isPublic: true }
     },
     {
       path: "/about",
       name: "about",
       component: About,
+      meta: { isPublic: true }
     },
     {
       path: "/calendar",
@@ -32,12 +35,24 @@ const router = new Router({
       path: "/login",
       name: "login",
       component: Login,
+      meta: { isPublic: true }
     },
     {
       path: "/signup",
       name: "signup",
       component: Signup,
+      meta: { isPublic: true }
     }
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  // isPublic でない場合(=認証が必要な場合)、かつ、ログインしていない場合
+  if (to.matched.some(record => !record.meta.isPublic) && !store.state.user) {
+    next({ path: '/login' });
+  } else {
+    next();
+  }
+});
+
 export default router;
